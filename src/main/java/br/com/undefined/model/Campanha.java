58871559dfb3c694e646f63b47dd446f18a7cd06
Joaquim Neto <jsn.piata@gmail.com>
2016-01-27 02:34:35 -0200

    adjusted JPA config

--- src/main/java/br/com/undefined/model/Campanha.java
+++ src/main/java/br/com/undefined/model/Campanha.java
@@ -3,14 +3,28 @@ package br.com.undefined.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "campanha")
 public class Campanha implements Serializable {
 	private static final long serialVersionUID = -226535293720997217L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_campanha")
 	private int idCampanha;
+	
 	private String nome;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date inicio;
 	private int duracao;
 	
